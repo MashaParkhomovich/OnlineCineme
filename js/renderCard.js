5ecda942a5b61b2c35dc40db0f3452ab8a9017de
Maria <masha.parkhomovich.03@mail.ru>
2021-12-12 22:28:08 +0300

    work with rating

--- js/renderCard.js
+++ js/renderCard.js
@@ -13,11 +13,7 @@ const renderCard = (data) => {
     const link = document.createElement('a');
     link.className = "other-films__link";
   
-    if(item.vote_average == 0){
-      link.dataset.rating = '—';
-    } else{
-      link.dataset.rating = item.vote_average; 
-    }
+    if(item.vote_average) link.dataset.rating = item.vote_average;
 
     const img = document.createElement('img');
     img.className = "other-films__img";
