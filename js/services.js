f3c1f251d673353f146ecff4cac9849071cb3b2c
Maria <masha.parkhomovich.03@mail.ru>
2021-12-12 03:35:39 +0300

    work with API

---
+++ js/services.js
@@ -0,0 +1,25 @@
+const API_KEY = 'e697b453087c50669357f7bb4bb06443';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const LANGUAGE = '&language=ru-RU';
+
+// https://api.themoviedb.org/3/movie/550?api_key=e697b453087c50669357f7bb4bb06443
+
+const getData = url => {
+  return fetch(url)
+    .then(response => {
+      if(response.ok){
+        return response.json()
+      }
+      throw `Что-то пошло не так, ошибка ${response.status}`
+    })
+    
+    .catch(err => console.error(err))
+};
+
+
+
+
+export const getTriends = async (type = 'all', period = 'day', page = 1) => {
+  const url = `${BASE_URL}/trending/${type}/${period}?api_key=${API_KEY}${LANGUAGE}&page = ${page}`;
+  return await getData(url);  
+};
